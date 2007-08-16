5fb60d73ce28df2a8c1318ea58d2f6b76ff88cc1
shane <shane@promode.org>
2007-08-16 08:38:49 +0000

    Early out AABB collision optimisation from Robert Beckebans (Xreal)
    git-svn-id: https://svn.mode-x.org/cnq3/trunk@885 df7340c0-8d17-0410-b57c-d8c7d96bbbdc

--- code/qcommon/q_shared.h
+++ code/qcommon/q_shared.h
@@ -566,6 +566,13 @@ void AxisCopy( vec3_t in[3], vec3_t out[3] );
 void SetPlaneSignbits( struct cplane_s *out );
 int BoxOnPlaneSide (vec3_t emins, vec3_t emaxs, struct cplane_s *plane);
 
+qboolean BoundsIntersect(const vec3_t mins, const vec3_t maxs,
+		const vec3_t mins2, const vec3_t maxs2);
+qboolean BoundsIntersectSphere(const vec3_t mins, const vec3_t maxs,
+		const vec3_t origin, vec_t radius);
+qboolean BoundsIntersectPoint(const vec3_t mins, const vec3_t maxs,
+		const vec3_t origin);
+
 float	AngleMod(float a);
 float	LerpAngle (float from, float to, float frac);
 float	AngleSubtract( float a1, float a2 );
