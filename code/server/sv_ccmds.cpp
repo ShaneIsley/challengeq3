57645f8a195ca4b4a9837f30cf8e48926c44320b
arQon <arqon@promode.org>
2007-11-29 04:25:55 +0000

    git-svn-id: https://svn.mode-x.org/cnq3/trunk@1181 df7340c0-8d17-0410-b57c-d8c7d96bbbdc

--- code/server/sv_ccmds.cpp
+++ code/server/sv_ccmds.cpp
@@ -156,7 +156,7 @@ static void SV_Map_f( void )
 	}
 
 	// force latched values to get set
-	Cvar_Get( "g_gametype", "0", CVAR_SERVERINFO | CVAR_USERINFO | CVAR_LATCH );
+	Cvar_Get( "g_gametype", "0", CVAR_SERVERINFO | CVAR_LATCH );
 
 	const char* cmd = Cmd_Argv(0);
 	if( Q_stricmpn( cmd, "sp", 2 ) == 0 ) {
