42798a2c30fd6c2290d64064fe018e0585170c59
arQon <arqon@promode.org>
2007-12-18 03:01:32 +0000

    git-svn-id: https://svn.mode-x.org/cnq3/trunk@1236 df7340c0-8d17-0410-b57c-d8c7d96bbbdc

--- code/client/cl_ui.cpp
+++ code/client/cl_ui.cpp
@@ -1026,36 +1026,6 @@ static intptr_t CL_UISystemCalls( intptr_t* args )
 		re.RegisterFont( VMA(1), args[2], VMA(3));
 		return 0;
 
-	case UI_MEMSET:
-		Com_Memset( VMA(1), args[2], args[3] );
-		return 0;
-
-	case UI_MEMCPY:
-		Com_Memcpy( VMA(1), VMA(2), args[3] );
-		return 0;
-
-	case UI_STRNCPY:
-		strncpy( VMA(1), VMA(2), args[3] );
-		return args[1];
-
-	case UI_SIN:
-		return FloatAsInt( sin( VMF(1) ) );
-
-	case UI_COS:
-		return FloatAsInt( cos( VMF(1) ) );
-
-	case UI_ATAN2:
-		return FloatAsInt( atan2( VMF(1), VMF(2) ) );
-
-	case UI_SQRT:
-		return FloatAsInt( sqrt( VMF(1) ) );
-
-	case UI_FLOOR:
-		return FloatAsInt( floor( VMF(1) ) );
-
-	case UI_CEIL:
-		return FloatAsInt( ceil( VMF(1) ) );
-
 /*
 	case UI_PC_ADD_GLOBAL_DEFINE:
 		return botlib_export->PC_AddGlobalDefine( VMA(1) );
