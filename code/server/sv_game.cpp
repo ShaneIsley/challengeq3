2a82a0043d50661f5f46397ed1f9bd869e01a534
arQon <arqon@promode.org>
2007-12-18 05:16:28 +0000

    git-svn-id: https://svn.mode-x.org/cnq3/trunk@1244 df7340c0-8d17-0410-b57c-d8c7d96bbbdc

--- code/server/sv_game.cpp
+++ code/server/sv_game.cpp
@@ -766,11 +766,11 @@ static intptr_t SV_GameSystemCalls( intptr_t* args )
 
 	case TRAP_SQRT:
 		return FloatAsInt( sqrt( VMF(1) ) );
-/*
+
 	case TRAP_MATRIXMULTIPLY:
 		MatrixMultiply( VMA(1), VMA(2), VMA(3) );
 		return 0;
-*/
+
 	case TRAP_ANGLEVECTORS:
 		AngleVectors( VMA(1), VMA(2), VMA(3), VMA(4) );
 		return 0;
