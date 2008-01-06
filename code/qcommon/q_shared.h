abf89911b7429191160b250a0dee3125e3f3dd2d
arQon <arqon@promode.org>
2008-01-06 10:38:41 +0000

    git-svn-id: https://svn.mode-x.org/cnq3/trunk@1260 df7340c0-8d17-0410-b57c-d8c7d96bbbdc

--- code/qcommon/q_shared.h
+++ code/qcommon/q_shared.h
@@ -138,21 +138,21 @@ typedef qbool qboolean;
 
 //=============================================================
 
-#ifdef Q3_VM
+#if defined(Q3_VM)
 	typedef int intptr_t;
+#elif defined(_MSC_VER)
+	#include <stddef.h>
+	typedef __int64 int64_t;
+	typedef __int32 int32_t;
+	typedef __int16 int16_t;
+	typedef __int8 int8_t;
+	typedef unsigned __int64 uint64_t;
+	typedef unsigned __int32 uint32_t;
+	typedef unsigned __int16 uint16_t;
+	typedef unsigned __int8 uint8_t;
+	typedef ptrdiff_t intptr_t;
 #else
-# ifndef _MSC_VER
-#  include <stdint.h>
-# else
-   typedef __int64 int64_t;
-   typedef __int32 int32_t;
-   typedef __int16 int16_t;
-   typedef __int8 int8_t;
-   typedef unsigned __int64 uint64_t;
-   typedef unsigned __int32 uint32_t;
-   typedef unsigned __int16 uint16_t;
-   typedef unsigned __int8 uint8_t;
-# endif
+	#include <stdint.h>
 #endif
 
 typedef unsigned char		byte;
