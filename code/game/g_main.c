bbb4bcf5bc704cec912cef18436e2931b15a6707
arQon <arqon@promode.org>
2008-01-12 23:46:44 +0000

    git-svn-id: https://svn.mode-x.org/cnq3/trunk@1271 df7340c0-8d17-0410-b57c-d8c7d96bbbdc

--- code/game/g_main.c
+++ code/game/g_main.c
@@ -149,7 +149,7 @@ static cvarTable_t		gameCvarTable[] = {
 	{ &g_cubeTimeout, "g_cubeTimeout", "30", 0, 0, qfalse },
 	{ &g_redteam, "g_redteam", "Stroggs", CVAR_ARCHIVE | CVAR_SERVERINFO | CVAR_USERINFO , 0, qtrue, qtrue },
 	{ &g_blueteam, "g_blueteam", "Pagans", CVAR_ARCHIVE | CVAR_SERVERINFO | CVAR_USERINFO , 0, qtrue, qtrue  },
-	{ &g_singlePlayer, "ui_singlePlayerActive", "", 0, 0, qfalse, qfalse  },
+	{ &g_singlePlayer, "sv_singlePlayer", "", 0, 0, qfalse, qfalse  },
 
 	{ &g_enableDust, "g_enableDust", "0", CVAR_SERVERINFO, 0, qtrue, qfalse },
 	{ &g_enableBreath, "g_enableBreath", "0", CVAR_SERVERINFO, 0, qtrue, qfalse },
@@ -831,7 +831,7 @@ void BeginIntermission()
 
 #ifdef MISSIONPACK
 	if (g_singlePlayer.integer) {
-		trap_Cvar_Set("ui_singlePlayerActive", "0");
+		trap_Cvar_Set("sv_singlePlayer", "0");
 		UpdateTournamentInfo();
 	}
 #else
