134fa32e7abaff12b80837cd4520a09d0f3ea5ec
arQon <arqon@promode.org>
2008-02-22 11:45:29 +0000

    git-svn-id: https://svn.mode-x.org/cnq3/trunk@1342 df7340c0-8d17-0410-b57c-d8c7d96bbbdc

--- code/unix/unix_shared.cpp
+++ code/unix/unix_shared.cpp
@@ -51,14 +51,15 @@ unsigned long sys_timeBase = 0;
    NOTE: sys_timeBase*1000 + curtime -> ms since the Epoch
      0x7fffffff ms - ~24 days
    although timeval:tv_usec is an int, I'm not sure wether it is actually used as an unsigned int
-     (which would affect the wrap period) */
-unsigned long curtime;
-unsigned long	Sys_Milliseconds ()
+     (which would affect the wrap period)
+*/
+int Sys_Milliseconds()
 {
-	struct timeval tp;
+	static int curtime;
 
+	struct timeval tp;
 	gettimeofday(&tp, NULL);
-	
+
 	if (!sys_timeBase)
 	{
 		sys_timeBase = tp.tv_sec;
@@ -66,7 +67,7 @@ unsigned long	Sys_Milliseconds ()
 	}
 
 	curtime = (tp.tv_sec - sys_timeBase)*1000 + tp.tv_usec/1000;
-	
+
 	return curtime;
 }
 
