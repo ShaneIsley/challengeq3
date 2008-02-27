c06ed8b73113b5304bd2c2e9247aabff226cbb12
arQon <arqon@promode.org>
2008-02-27 09:22:01 +0000

    git-svn-id: https://svn.mode-x.org/cnq3/trunk@1372 df7340c0-8d17-0410-b57c-d8c7d96bbbdc

--- code/qcommon/g_syscalls.c
+++ code/qcommon/g_syscalls.c
@@ -20,13 +20,17 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ===========================================================================
 */
 //
-#include "g_local.h"
 
 // this file is only included when building a dll
 // g_syscalls.asm is included instead when building a qvm
 #ifdef Q3_VM
 #error "Do not use in VM build"
 #endif
+
+
+#include "q_shared.h"
+#include "g_public.h"
+
 
 static intptr_t (QDECL *syscall)( intptr_t arg, ... ) = (intptr_t (QDECL *)( intptr_t, ...))-1;
 
