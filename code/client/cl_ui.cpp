2b5b2e7d40ba8b712a6adcf5b16ab366a471fb5d
arQon <arqon@promode.org>
2008-02-27 13:38:25 +0000

    git-svn-id: https://svn.mode-x.org/cnq3/trunk@1399 df7340c0-8d17-0410-b57c-d8c7d96bbbdc

--- code/client/cl_ui.cpp
+++ code/client/cl_ui.cpp
@@ -1050,19 +1050,6 @@ static intptr_t CL_UISystemCalls( intptr_t* args )
 	case UI_CEIL:
 		return FloatAsInt( ceil( VMF(1) ) );
 
-/*
-	case UI_PC_ADD_GLOBAL_DEFINE:
-		return botlib_export->PC_AddGlobalDefine( VMA(1) );
-	case UI_PC_LOAD_SOURCE:
-		return botlib_export->PC_LoadSourceHandle( VMA(1) );
-	case UI_PC_FREE_SOURCE:
-		return botlib_export->PC_FreeSourceHandle( args[1] );
-	case UI_PC_READ_TOKEN:
-		return botlib_export->PC_ReadTokenHandle( args[1], VMA(2) );
-	case UI_PC_SOURCE_FILE_AND_LINE:
-		return botlib_export->PC_SourceFileAndLine( args[1], VMA(2), VMA(3) );
-*/
-
 	case UI_S_STOPBACKGROUNDTRACK:
 		S_StopBackgroundTrack();
 		return 0;
