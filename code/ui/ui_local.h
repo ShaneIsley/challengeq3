44bbf933fb2a22e0512170b62fbea77873869c06
arQon <arqon@promode.org>
2008-02-27 06:52:45 +0000

    git-svn-id: https://svn.mode-x.org/cnq3/trunk@1363 df7340c0-8d17-0410-b57c-d8c7d96bbbdc

--- code/ui/ui_local.h
+++ code/ui/ui_local.h
@@ -980,7 +980,7 @@ int				trap_LAN_CompareServers( int source, int sortKey, int sortDir, int s1, in
 int				trap_MemoryRemaining( void );
 void			trap_GetCDKey( char *buf, int buflen );
 void			trap_SetCDKey( char *buf );
-void			trap_R_RegisterFont(const char *pFontname, int pointSize, fontInfo_t *font);
+qboolean		trap_R_RegisterFont( const char* fontName, int pointSize, fontInfo_t* font );
 void			trap_S_StopBackgroundTrack( void );
 void			trap_S_StartBackgroundTrack( const char *intro, const char *loop);
 int				trap_CIN_PlayCinematic( const char *arg0, int xpos, int ypos, int width, int height, int bits);
