bedf4ee92abcb5ee85da4e9b470a8947196ca10d
arQon <arqon@promode.org>
2008-03-03 07:38:50 +0000

    git-svn-id: https://svn.mode-x.org/cnq3/trunk@1471 df7340c0-8d17-0410-b57c-d8c7d96bbbdc

--- code/renderer/tr_sky.cpp
+++ code/renderer/tr_sky.cpp
@@ -466,43 +466,15 @@ static void FillCloudySkySide( const int mins[2], const int maxs[2], qbool addIn
 	}
 }
 
-static void FillCloudBox( const shader_t *shader, int stage )
-{
-	int i;
 
-	for ( i =0; i < 6; i++ )
+static void FillCloudBox( const shader_t* shader, int stage )
+{
+	// skybox surfs are ordered RBLFUD, so don't draw clouds on the last one
+
+	for (int i = 0; i < 5; ++i)
 	{
 		int sky_mins_subd[2], sky_maxs_subd[2];
 		int s, t;
-		float MIN_T;
-
-		if ( 1 ) // FIXME? shader->sky.fullClouds )
-		{
-			MIN_T = -HALF_SKY_SUBDIVISIONS;
-
-			// still don't want to draw the bottom, even if fullClouds
-			if ( i == 5 )
-				continue;
-		}
-		else
-		{
-			switch( i )
-			{
-			case 0:
-			case 1:
-			case 2:
-			case 3:
-				MIN_T = -1;
-				break;
-			case 5:
-				// don't draw clouds beneath you
-				continue;
-			case 4:		// top
-			default:
-				MIN_T = -HALF_SKY_SUBDIVISIONS;
-				break;
-			}
-		}
 
 		sky_mins[0][i] = floor( sky_mins[0][i] * HALF_SKY_SUBDIVISIONS ) / HALF_SKY_SUBDIVISIONS;
 		sky_mins[1][i] = floor( sky_mins[1][i] * HALF_SKY_SUBDIVISIONS ) / HALF_SKY_SUBDIVISIONS;
@@ -515,28 +487,10 @@ static void FillCloudBox( const shader_t *shader, int stage )
 			continue;
 		}
 
-		sky_mins_subd[0] = myftol( sky_mins[0][i] * HALF_SKY_SUBDIVISIONS );
-		sky_mins_subd[1] = myftol( sky_mins[1][i] * HALF_SKY_SUBDIVISIONS );
-		sky_maxs_subd[0] = myftol( sky_maxs[0][i] * HALF_SKY_SUBDIVISIONS );
-		sky_maxs_subd[1] = myftol( sky_maxs[1][i] * HALF_SKY_SUBDIVISIONS );
-
-		if ( sky_mins_subd[0] < -HALF_SKY_SUBDIVISIONS ) 
-			sky_mins_subd[0] = -HALF_SKY_SUBDIVISIONS;
-		else if ( sky_mins_subd[0] > HALF_SKY_SUBDIVISIONS ) 
-			sky_mins_subd[0] = HALF_SKY_SUBDIVISIONS;
-		if ( sky_mins_subd[1] < MIN_T )
-			sky_mins_subd[1] = MIN_T;
-		else if ( sky_mins_subd[1] > HALF_SKY_SUBDIVISIONS ) 
-			sky_mins_subd[1] = HALF_SKY_SUBDIVISIONS;
-
-		if ( sky_maxs_subd[0] < -HALF_SKY_SUBDIVISIONS ) 
-			sky_maxs_subd[0] = -HALF_SKY_SUBDIVISIONS;
-		else if ( sky_maxs_subd[0] > HALF_SKY_SUBDIVISIONS ) 
-			sky_maxs_subd[0] = HALF_SKY_SUBDIVISIONS;
-		if ( sky_maxs_subd[1] < MIN_T )
-			sky_maxs_subd[1] = MIN_T;
-		else if ( sky_maxs_subd[1] > HALF_SKY_SUBDIVISIONS ) 
-			sky_maxs_subd[1] = HALF_SKY_SUBDIVISIONS;
+		sky_mins_subd[0] = HALF_SKY_SUBDIVISIONS * Com_Clamp( -1, 1, sky_mins[0][i] );
+		sky_mins_subd[1] = HALF_SKY_SUBDIVISIONS * Com_Clamp( -1, 1, sky_mins[1][i] );
+		sky_maxs_subd[0] = HALF_SKY_SUBDIVISIONS * Com_Clamp( -1, 1, sky_maxs[0][i] );
+		sky_maxs_subd[1] = HALF_SKY_SUBDIVISIONS * Com_Clamp( -1, 1, sky_maxs[1][i] );
 
 		//
 		// iterate through the subdivisions
