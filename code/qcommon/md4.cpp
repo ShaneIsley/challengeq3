aff30b3beec99c1868a9b7434fc6cd4a75e762a2
arQon <arqon@promode.org>
2008-04-07 23:36:53 +0000

    git-svn-id: https://svn.mode-x.org/cnq3/trunk@1553 df7340c0-8d17-0410-b57c-d8c7d96bbbdc

--- code/qcommon/md4.cpp
+++ code/qcommon/md4.cpp
@@ -1,6 +1,4 @@
 /*
-	mdfour.c
-
 	An implementation of MD4 designed for use in the samba SMB
 	authentication protocol
 
@@ -23,25 +21,18 @@
 		Free Software Foundation, Inc.
 		59 Temple Place - Suite 330
 		Boston, MA  02111-1307, USA
-
-	$Id: mdfour.c,v 1.1 2002/08/23 22:03:27 abster Exp $
 */
+
+// NOTE: This code makes no attempt to be fast!
+// It assumes that a int is at least 32 bits long
 
 #include "q_shared.h"
 #include "qcommon.h"
 
-struct mdfour {
+struct md4ctx {
 	uint32_t A, B, C, D;
 	uint32_t totalN;
 };
-
-
-/* NOTE: This code makes no attempt to be fast!
-
-   It assumes that a int is at least 32 bits long
-*/
-
-static struct mdfour *m;
 
 #define F(X,Y,Z) (((X)&(Y)) | ((~(X))&(Z)))
 #define G(X,Y,Z) (((X)&(Y)) | ((X)&(Z)) | ((Y)&(Z)))
@@ -49,11 +40,11 @@ static struct mdfour *m;
 #define lshift(x,s) (((x)<<(s)) | ((x)>>(32-(s))))
 
 #define ROUND1(a,b,c,d,k,s) a = lshift(a + F(b,c,d) + X[k], s)
-#define ROUND2(a,b,c,d,k,s) a = lshift(a + G(b,c,d) + X[k] + 0x5A827999,s)
-#define ROUND3(a,b,c,d,k,s) a = lshift(a + H(b,c,d) + X[k] + 0x6ED9EBA1,s)
+#define ROUND2(a,b,c,d,k,s) a = lshift(a + G(b,c,d) + X[k] + 0x5A827999, s)
+#define ROUND3(a,b,c,d,k,s) a = lshift(a + H(b,c,d) + X[k] + 0x6ED9EBA1, s)
 
-/* this applies md4 to 64 byte chunks */
-static void mdfour64(uint32_t *M)
+// this applies md4 to 64 byte chunks
+static void mdfour64( md4ctx& md, const uint32_t* M )
 {
 	int j;
 	uint32_t AA, BB, CC, DD;
@@ -63,34 +54,34 @@ static void mdfour64(uint32_t *M)
 	for (j=0;j<16;j++)
 		X[j] = M[j];
 
-	A = m->A; B = m->B; C = m->C; D = m->D;
+	A = md.A; B = md.B; C = md.C; D = md.D;
 	AA = A; BB = B; CC = C; DD = D;
 
-        ROUND1(A,B,C,D,  0,  3);  ROUND1(D,A,B,C,  1,  7);
+	ROUND1(A,B,C,D,  0,  3);  ROUND1(D,A,B,C,  1,  7);
 	ROUND1(C,D,A,B,  2, 11);  ROUND1(B,C,D,A,  3, 19);
-        ROUND1(A,B,C,D,  4,  3);  ROUND1(D,A,B,C,  5,  7);
+	ROUND1(A,B,C,D,  4,  3);  ROUND1(D,A,B,C,  5,  7);
 	ROUND1(C,D,A,B,  6, 11);  ROUND1(B,C,D,A,  7, 19);
-        ROUND1(A,B,C,D,  8,  3);  ROUND1(D,A,B,C,  9,  7);
+	ROUND1(A,B,C,D,  8,  3);  ROUND1(D,A,B,C,  9,  7);
 	ROUND1(C,D,A,B, 10, 11);  ROUND1(B,C,D,A, 11, 19);
-        ROUND1(A,B,C,D, 12,  3);  ROUND1(D,A,B,C, 13,  7);
+	ROUND1(A,B,C,D, 12,  3);  ROUND1(D,A,B,C, 13,  7);
 	ROUND1(C,D,A,B, 14, 11);  ROUND1(B,C,D,A, 15, 19);
 
-        ROUND2(A,B,C,D,  0,  3);  ROUND2(D,A,B,C,  4,  5);
+	ROUND2(A,B,C,D,  0,  3);  ROUND2(D,A,B,C,  4,  5);
 	ROUND2(C,D,A,B,  8,  9);  ROUND2(B,C,D,A, 12, 13);
-        ROUND2(A,B,C,D,  1,  3);  ROUND2(D,A,B,C,  5,  5);
+	ROUND2(A,B,C,D,  1,  3);  ROUND2(D,A,B,C,  5,  5);
 	ROUND2(C,D,A,B,  9,  9);  ROUND2(B,C,D,A, 13, 13);
-        ROUND2(A,B,C,D,  2,  3);  ROUND2(D,A,B,C,  6,  5);
+	ROUND2(A,B,C,D,  2,  3);  ROUND2(D,A,B,C,  6,  5);
 	ROUND2(C,D,A,B, 10,  9);  ROUND2(B,C,D,A, 14, 13);
-        ROUND2(A,B,C,D,  3,  3);  ROUND2(D,A,B,C,  7,  5);
+	ROUND2(A,B,C,D,  3,  3);  ROUND2(D,A,B,C,  7,  5);
 	ROUND2(C,D,A,B, 11,  9);  ROUND2(B,C,D,A, 15, 13);
 
 	ROUND3(A,B,C,D,  0,  3);  ROUND3(D,A,B,C,  8,  9);
 	ROUND3(C,D,A,B,  4, 11);  ROUND3(B,C,D,A, 12, 15);
-        ROUND3(A,B,C,D,  2,  3);  ROUND3(D,A,B,C, 10,  9);
+	ROUND3(A,B,C,D,  2,  3);  ROUND3(D,A,B,C, 10,  9);
 	ROUND3(C,D,A,B,  6, 11);  ROUND3(B,C,D,A, 14, 15);
-        ROUND3(A,B,C,D,  1,  3);  ROUND3(D,A,B,C,  9,  9);
+	ROUND3(A,B,C,D,  1,  3);  ROUND3(D,A,B,C,  9,  9);
 	ROUND3(C,D,A,B,  5, 11);  ROUND3(B,C,D,A, 13, 15);
-        ROUND3(A,B,C,D,  3,  3);  ROUND3(D,A,B,C, 11,  9);
+	ROUND3(A,B,C,D,  3,  3);  ROUND3(D,A,B,C, 11,  9);
 	ROUND3(C,D,A,B,  7, 11);  ROUND3(B,C,D,A, 15, 15);
 
 	A += AA; B += BB; C += CC; D += DD;
@@ -98,10 +89,10 @@ static void mdfour64(uint32_t *M)
 	for (j=0;j<16;j++)
 		X[j] = 0;
 
-	m->A = A; m->B = B; m->C = C; m->D = D;
+	md.A = A; md.B = B; md.C = C; md.D = D;
 }
 
-static void copy64(uint32_t *M, byte *in)
+static void copy64( uint32_t* M, const byte* in )
 {
 	int i;
 
@@ -110,33 +101,32 @@ static void copy64(uint32_t *M, byte *in)
 			(in[i*4+1]<<8) | (in[i*4+0]<<0);
 }
 
-static void copy4(byte *out,uint32_t x)
+static void copy4( byte* out, uint32_t x )
 {
-	out[0] = x&0xFF;
-	out[1] = (x>>8)&0xFF;
-	out[2] = (x>>16)&0xFF;
-	out[3] = (x>>24)&0xFF;
+	out[0] = x & 0xFF;
+	out[1] = (x>>8) & 0xFF;
+	out[2] = (x>>16) & 0xFF;
+	out[3] = (x>>24) & 0xFF;
 }
 
-void mdfour_begin(struct mdfour *md)
+static void mdfour_begin( md4ctx& md )
 {
-	md->A = 0x67452301;
-	md->B = 0xefcdab89;
-	md->C = 0x98badcfe;
-	md->D = 0x10325476;
-	md->totalN = 0;
+	md.A = 0x67452301;
+	md.B = 0xefcdab89;
+	md.C = 0x98badcfe;
+	md.D = 0x10325476;
+	md.totalN = 0;
 }
 
-
-static void mdfour_tail(byte *in, int n)
+static void mdfour_tail( md4ctx& md, const byte* in, int n )
 {
 	byte buf[128];
 	uint32_t M[16];
 	uint32_t b;
 
-	m->totalN += n;
+	md.totalN += n;
 
-	b = m->totalN * 8;
+	b = md.totalN * 8;
 
 	Com_Memset(buf, 0, 128);
 	if (n) Com_Memcpy(buf, in, n);
@@ -145,64 +135,54 @@ static void mdfour_tail(byte *in, int n)
 	if (n <= 55) {
 		copy4(buf+56, b);
 		copy64(M, buf);
-		mdfour64(M);
+		mdfour64(md, M);
 	} else {
 		copy4(buf+120, b);
 		copy64(M, buf);
-		mdfour64(M);
+		mdfour64(md, M);
 		copy64(M, buf+64);
-		mdfour64(M);
+		mdfour64(md, M);
 	}
 }
 
-static void mdfour_update(struct mdfour *md, byte *in, int n)
+static void mdfour_update( md4ctx& md, const byte* in, int n )
 {
 	uint32_t M[16];
 
-	m = md;
-
-	if (n == 0) mdfour_tail(in, n);
+	if (n == 0) mdfour_tail(md, in, n);
 
 	while (n >= 64) {
 		copy64(M, in);
-		mdfour64(M);
+		mdfour64(md, M);
 		in += 64;
 		n -= 64;
-		m->totalN += 64;
+		md.totalN += 64;
 	}
 
-	mdfour_tail(in, n);
+	mdfour_tail(md, in, n);
 }
 
-
-static void mdfour_result(struct mdfour *md, byte *out)
+static void mdfour_result( md4ctx& md, byte* out )
 {
-	m = md;
-
-	copy4(out, m->A);
-	copy4(out+4, m->B);
-	copy4(out+8, m->C);
-	copy4(out+12, m->D);
+	copy4(out, md.A);
+	copy4(out+4, md.B);
+	copy4(out+8, md.C);
+	copy4(out+12, md.D);
 }
 
-static void mdfour(byte *out, byte *in, int n)
+static void mdfour( byte* out, const byte* in, int n )
 {
-	struct mdfour md;
-	mdfour_begin(&md);
-	mdfour_update(&md, in, n);
-	mdfour_result(&md, out);
+	md4ctx md;
+	mdfour_begin(md);
+	mdfour_update(md, in, n);
+	mdfour_result(md, out);
 }
 
-//===================================================================
+///////////////////////////////////////////////////////////////
 
-unsigned Com_BlockChecksum (const void *buffer, int length)
+unsigned Com_BlockChecksum( const void* buffer, int length )
 {
-	int				digest[4];
-	unsigned	val;
-
-	mdfour( (byte *)digest, (byte *)buffer, length );
-	
-	val = digest[0] ^ digest[1] ^ digest[2] ^ digest[3];
-
-	return val;
+	int digest[4];
+	mdfour( (byte*)digest, (const byte*)buffer, length );
+	return (unsigned)( digest[0] ^ digest[1] ^ digest[2] ^ digest[3] );
 }
